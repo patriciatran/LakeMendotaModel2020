8e761031c60db75d6ee9dc31503b682be39b59f1
patriciatran <contact.patriciatran@gmail.com>
2021-03-04 14:17:25 -0600

    loop to make figs

--- Code/entrainment.R
+++ Code/entrainment.R
@@ -122,143 +122,159 @@ my_theme <- theme_bw()+
         panel.background = element_blank(),
         panel.grid = element_blank())
 
-#layer.to.plot <- c("epi","hypo","mixed") # whole water column
 
-layer.to.plot <- c("epi","mixed") # epi layer only
+layer.to.plot1 <- c("epi","hypo","mixed") # whole water column
+
+layer.to.plot2 <- c("epi","mixed") # epi layer only
+
+layer.to.plot.list <- list(layer.to.plot1, layer.to.plot2)
 
 # variables I've plotted: start with those that are mg/L:
-#nh4_sloh, no3no2_sloh, totpuf_sloh, drp_sloh
-
-var.to.plot <- "no3no2_sloh"
+vars.to.plot.list <- c("nh4_sloh", "no3no2_sloh", "totpuf_sloh", "drp_sloh")
 
 
+for (i in 1:length(layer.to.plot.list)){
+  layer.to.plot <- layer.to.plot.list[i][[1]]
+  for (j in 1:length(vars.to.plot.list)){
+    #Let's try to calculate the load difference in nh4 in the whole water column between time steps:
+    #LTERnutrient.df.kz.test <- LTERnutrient.df.kz %>% filter(!is.na(var.to.plot))
+    #LTERnutrient.df.kz %>% nrow()
+    #LTERnutrient.df.kz.test <- LTERnutrient.df.kz %>% filter(!is.na(no3no2_sloh))
+    #LTERnutrient.df.kz %>% filter(!is.na(no3no2_sloh)) %>% nrow() 
+    #LTERnutrient.df.kz %>% filter(!is.na(var.to.plot)) %>% nrow() 
+    var.to.plot <- vars.to.plot.list[j]
+    
+    LTERnutrient.df.kz.test <- LTERnutrient.df.kz %>% filter(!is.na(!!as.symbol(var.to.plot))) %>%
+      filter(layer %in% layer.to.plot)
+    
+    # Save name of the layers so I can name my plots accordingly:
+    layer.names <- gsub(", ","",toString(layer.to.plot))
+    
+    nrow(LTERnutrient.df.kz.test)
+    
+    #LTERnutrient.df.kz.test <- LTERnutrient.df.kz.test %>% 
+    #  group_by(sampledate) %>% 
+    #  mutate(massArea = trapz(depth, !!as.symbol(var.to.plot)))
+    
+    
+    LTERnutrient.df.kz.test <- LTERnutrient.df.kz.test %>% 
+      group_by(sampledate) %>% 
+      mutate(massArea = trapz(depth, !!as.symbol(var.to.plot)))
+    
+    
+    # Quickly plot the values over time:
+    ggplot(LTERnutrient.df.kz.test)+
+      geom_point(aes(x=sampledate, y=massArea), color="blue", size=4, alpha=0.7)+
+      geom_point(aes(x=sampledate, y=!!as.symbol(var.to.plot)), color="red", size=4, alpha=0.7)+
+      ggtitle(paste0("Red=",var.to.plot,"\nBlue = Calculated change in concentrations over depth per time"))+
+      ylab("Red: [mass/time] , Blue [mg/L]")+
+      geom_hline(aes(yintercept=0))+
+      my_theme
+    
+    ggsave(paste0("Plots/Entrainment/calculation.mass.over.area.",var.to.plot,"_",layer.names,".PDF"), width = 11, height = 5, units = "in")
+    ggsave(paste0("Plots/Entrainment/calculation.mass.over.area.",var.to.plot,"_",layer.names,".png"), width = 11, height = 5, units = "in", dpi=300)
+    
+    #Now that we have our first equation, we should use that value to find the difference in that value between two time points.
+    LTERnutrient.df.kz.test.lag.day <- LTERnutrient.df.kz.test %>% select(sampledate, massArea)
+    LTERnutrient.df.kz.test.lag.day$sampledate <- ymd(LTERnutrient.df.kz.test.lag.day$sampledate)
+    # Convert to numeric dates:
+    
+    LTERnutrient.df.kz.test.lag.day$numericdates <- as.numeric(LTERnutrient.df.kz.test.lag.day$sampledate)
+    
+    # Remove duplicates:
+    LTERnutrient.df.kz.test.lag.day <- LTERnutrient.df.kz.test.lag.day %>% distinct()
+    
+    lagDays <- NA
+    
+    lagDays2 <- diff(LTERnutrient.df.kz.test.lag.day$numericdates, lag=1)
+    lagDays3 <- append(lagDays, lagDays2)
+    
+    lagDays3
+    
+    
+    LTERnutrient.df.kz.test.lag.day$lagDay <- lagDays3
+    
+    LTERnutrient.df.kz.test.unique <- LTERnutrient.df.kz.test.lag.day %>% select(sampledate, massArea, lagDay) %>% distinct()
+    
+    LTERnutrient.df.kz.test.unique$changeinLoad <- lag(LTERnutrient.df.kz.test.unique$massArea)/LTERnutrient.df.kz.test.lag.day$lagDay
+    
+    write.table(LTERnutrient.df.kz.test.unique, 
+                paste0("Data/LTERnutrient.df.kz.test.unique_",var.to.plot,"_",layer.names,".tsv"),
+                sep="\t", row.names=FALSE)
+    
+    ### NOTE ABOUT UNITS!!
+    # I'll change the code later to make sure the units are right but for now... whatever!
+    
+    ymin = min(LTERnutrient.df.kz.test.unique$changeinLoad, na.rm=T)
+    ymax = max(LTERnutrient.df.kz.test.unique$changeinLoad, na.rm=T)
+    
+    ggplot(LTERnutrient.df.kz.test.unique)+
+      geom_point(aes(x=sampledate, y=changeinLoad), size=4, alpha=0.7)+
+      ylab((paste0(var.to.plot, " in g ",expression(day^-1,m^2^-1))))+
+      my_theme+
+      ggtitle("Load difference in water column between time points")
+    
+    ggsave(paste0("Plots/Entrainment/",var.to.plot,"_",layer.names,".Load.diff.over.time.one.layer.PDF"), width = 11, height = 6, units = "in")
+    ggsave(paste0("Plots/Entrainment/",var.to.plot,"_",layer.names,".Load.diff.over.time.one.layer.png"),dpi=300, width = 11, height = 6, units = "in")
+    
+    min(LTERnutrient.df.kz.test.unique$sampledate)
+    max(LTERnutrient.df.kz.test.unique$sampledate)
+    
+    ice.data <- readRDS("Data/ice.data.RDS")
+    
+    ice.data$date <- as.Date(ice.data$value, origin=paste0(ice.data$year4,"-01-01"))
+    ice.on <- ice.data %>% filter(variable == "ice_on_doy")
+    ice.off <- ice.data %>% filter(variable == "ice_off_doy")
+    
+    ice.data.to.plot <- ice.data %>% filter(variable == "ice_on_doy" | variable == "ice_off_doy")
+    
+    ice.data.to.plot <- ice.data.to.plot %>% mutate(color.to.plot = ifelse(variable == "ice_on_doy","red","blue"))
+    
+    ## Adding stratification onset and off-set information:
+    stratification <- read.table("Data/start.onset.offset.TSV", sep="\t", header=TRUE)
+    stratification$Onset_date <- as.Date(stratification$Onset, origin=paste0(stratification$year,"-01-01"))
+    stratification$Offset_date <- as.Date(stratification$Offset, origin=paste0(stratification$year,"-01-01"))
+    
+    stratification <- stratification %>% filter(year > 2014)
+    
+    stratification <- stratification %>% gather(variable_strat, date_strat, Onset_date:Offset_date)
+    
+    
+    ggplot(LTERnutrient.df.kz.test.unique)+
+      geom_point(aes(x=sampledate, y=changeinLoad), size=4, alpha=0.7)+
+      ylab(expression(paste("in g ",day^-1,m^2^-1)))+
+      my_theme+
+      ggtitle("Load difference in water column between time points")+
+      annotate( #ice-on
+        "segment",
+        x=ice.data.to.plot$date,
+        xend=ice.data.to.plot$date,
+        y=ymax,
+        yend=0,
+        color=ice.data.to.plot$color.to.plot,
+        alpha=1,
+        linetype=2
+      )+
+      annotate( #stratification
+        "segment",
+        x=stratification$date_strat,
+        xend=stratification$date_strat,
+        y=ymax,
+        yend=0,
+        color="dark green",
+        alpha=1,
+        linetype=2
+      )+
+      ylim(ymin-(ymin/100*20),ymax+(ymax/100*20))+
+      ggtitle(paste0(as.character(var.to.plot),", layer=",layer.names),
+              subtitle = "Red line = Ice on, \nBlue off = Ice off, \nGreen = Stratification onset and offset")
+    
+    ggsave(paste0("Plots/Entrainment/ice.on.ice.off.strat.load.diff.",var.to.plot,"_",layer.names,".dashed.PDF"), width = 11, height = 8.5, units="in")
+    ggsave(paste0("Plots/Entrainment/ice.on.ice.off.strat.load.diff.",var.to.plot,"_",layer.names,".dashed.png"), dpi=300, width = 11, height = 8.5, units="in")
+  
+    print("Next variable")
+  }
+  print("Next Layer")
+}
 
-#Let's try to calculate the load difference in nh4 in the whole water column between time steps:
-#LTERnutrient.df.kz.test <- LTERnutrient.df.kz %>% filter(!is.na(var.to.plot))
-#LTERnutrient.df.kz %>% nrow()
-#LTERnutrient.df.kz.test <- LTERnutrient.df.kz %>% filter(!is.na(no3no2_sloh))
-#LTERnutrient.df.kz %>% filter(!is.na(no3no2_sloh)) %>% nrow() 
-#LTERnutrient.df.kz %>% filter(!is.na(var.to.plot)) %>% nrow() 
 
-LTERnutrient.df.kz.test <- LTERnutrient.df.kz %>% filter(!is.na(!!as.symbol(var.to.plot))) %>%
-  filter(layer %in% layer.to.plot)
-
-# Save name of the layers so I can name my plots accordingly:
-layer.names <- gsub(", ","",toString(layer.to.plot))
-
-nrow(LTERnutrient.df.kz.test)
-
-#LTERnutrient.df.kz.test <- LTERnutrient.df.kz.test %>% 
-#  group_by(sampledate) %>% 
-#  mutate(massArea = trapz(depth, !!as.symbol(var.to.plot)))
-
-
-LTERnutrient.df.kz.test <- LTERnutrient.df.kz.test %>% 
-  group_by(sampledate) %>% 
-  mutate(massArea = trapz(depth, !!as.symbol(var.to.plot)))
-
-
-# Quickly plot the values over time:
-ggplot(LTERnutrient.df.kz.test)+
-  geom_point(aes(x=sampledate, y=massArea), color="blue", size=4, alpha=0.7)+
-  geom_point(aes(x=sampledate, y=!!as.symbol(var.to.plot)), color="red", size=4, alpha=0.7)+
-  ggtitle(paste0("Red=",var.to.plot,"\nBlue = Calculated change in concentrations over depth per time"))+
-  ylab("Red: [mass/time] , Blue [mg/L]")+
-  my_theme
-
-ggsave(paste0("Plots/Entrainment/calculation.mass.over.area.",var.to.plot,"_",layer.names,".PDF"), width = 11, height = 5, units = "in")
-ggsave(paste0("Plots/Entrainment/calculation.mass.over.area.",var.to.plot,"_",layer.names,".png"), width = 11, height = 5, units = "in", dpi=300)
-
-#Now that we have our first equation, we should use that value to find the difference in that value between two time points.
-LTERnutrient.df.kz.test.lag.day <- LTERnutrient.df.kz.test %>% select(sampledate, massArea)
-LTERnutrient.df.kz.test.lag.day$sampledate <- ymd(LTERnutrient.df.kz.test.lag.day$sampledate)
-# Convert to numeric dates:
-
-LTERnutrient.df.kz.test.lag.day$numericdates <- as.numeric(LTERnutrient.df.kz.test.lag.day$sampledate)
-
-# Remove duplicates:
-LTERnutrient.df.kz.test.lag.day <- LTERnutrient.df.kz.test.lag.day %>% distinct()
-
-lagDays <- NA
-
-lagDays2 <- diff(LTERnutrient.df.kz.test.lag.day$numericdates, lag=1)
-lagDays3 <- append(lagDays, lagDays2)
-
-lagDays3
-
-
-LTERnutrient.df.kz.test.lag.day$lagDay <- lagDays3
-
-LTERnutrient.df.kz.test.unique <- LTERnutrient.df.kz.test.lag.day %>% select(sampledate, massArea, lagDay) %>% distinct()
-
-LTERnutrient.df.kz.test.unique$changeinLoad <- lag(LTERnutrient.df.kz.test.unique$massArea)/LTERnutrient.df.kz.test.lag.day$lagDay
-
-### NOTE ABOUT UNITS!!
-# I'll change the code later to make sure the units are right but for now... whatever!
-
-ymin = min(LTERnutrient.df.kz.test.unique$changeinLoad, na.rm=T)
-ymax = max(LTERnutrient.df.kz.test.unique$changeinLoad, na.rm=T)
-
-ggplot(LTERnutrient.df.kz.test.unique)+
-  geom_point(aes(x=sampledate, y=changeinLoad), size=4, alpha=0.7)+
-  ylab((paste0(var.to.plot, " in g ",expression(day^-1,m^2^-1))))+
-  my_theme+
-  ggtitle("Load difference in water column between time points")
-
-ggsave(paste0("Plots/Entrainment/",var.to.plot,"_",layer.names,".Load.diff.over.time.one.layer.PDF"), width = 11, height = 6, units = "in")
-ggsave(paste0("Plots/Entrainment/",var.to.plot,"_",layer.names,".Load.diff.over.time.one.layer.png"),dpi=300, width = 11, height = 6, units = "in")
-
-min(LTERnutrient.df.kz.test.unique$sampledate)
-max(LTERnutrient.df.kz.test.unique$sampledate)
-
-ice.data <- readRDS("Data/ice.data.RDS")
-
-ice.data$date <- as.Date(ice.data$value, origin=paste0(ice.data$year4,"-01-01"))
-ice.on <- ice.data %>% filter(variable == "ice_on_doy")
-ice.off <- ice.data %>% filter(variable == "ice_off_doy")
-
-ice.data.to.plot <- ice.data %>% filter(variable == "ice_on_doy" | variable == "ice_off_doy")
-
-ice.data.to.plot <- ice.data.to.plot %>% mutate(color.to.plot = ifelse(variable == "ice_on_doy","red","blue"))
-
-## Adding stratification onset and off-set information:
-stratification <- read.table("Data/start.onset.offset.TSV", sep="\t", header=TRUE)
-stratification$Onset_date <- as.Date(stratification$Onset, origin=paste0(stratification$year,"-01-01"))
-stratification$Offset_date <- as.Date(stratification$Offset, origin=paste0(stratification$year,"-01-01"))
-
-stratification <- stratification %>% filter(year > 2014)
-
-stratification <- stratification %>% gather(variable_strat, date_strat, Onset_date:Offset_date)
-
-
-ggplot(LTERnutrient.df.kz.test.unique)+
-  geom_point(aes(x=sampledate, y=changeinLoad), size=4, alpha=0.7)+
-  ylab(expression(paste("in g ",day^-1,m^2^-1)))+
-  my_theme+
-  ggtitle("Load difference in water column between time points")+
-  annotate( #ice-on
-    "segment",
-    x=ice.data.to.plot$date,
-    xend=ice.data.to.plot$date,
-    y=ymax,
-    yend=0,
-    color=ice.data.to.plot$color.to.plot,
-    alpha=1,
-    linetype=2
-  )+
-  annotate( #stratification
-    "segment",
-    x=stratification$date_strat,
-    xend=stratification$date_strat,
-    y=ymax,
-    yend=0,
-    color="dark green",
-    alpha=1,
-    linetype=2
-  )+
-  ylim(ymin-(ymin/100*20),ymax+(ymax/100*20))+
-  ggtitle(paste0(as.character(var.to.plot),", layer=",layer.names),
-    subtitle = "Red line = Ice on, \nBlue off = Ice off, \nGreen = Stratification onset and offset")
-
-ggsave(paste0("Plots/Entrainment/ice.on.ice.off.strat.load.diff.",var.to.plot,"_",layer.names,".dashed.PDF"), width = 11, height = 8.5, units="in")
-ggsave(paste0("Plots/Entrainment/ice.on.ice.off.strat.load.diff.",var.to.plot,"_",layer.names,".dashed.png"), dpi=300, width = 11, height = 8.5, units="in")
